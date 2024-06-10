dee904492ab94c558bf2bf78bd935ce97965e8b3
Mathys Langiny <mathys.langiny@etudiant.univ-reims.fr>
2024-06-10 10:18:19 +0200

    Création de la classe Poster.php avec ces accesseurs getId & getJpeg en publique.

---
+++ src/Entity/Poster.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class Poster
+{
+    private int $id;
+    private string $jpeg;
+
+    /**
+     * Accesseur de l'id de l'instance Poster. Renvoie un entier?
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Accesseur renvoyant une chaine de caractère, c'est le jpeg de l'instance Poster
+     *
+     * @return string
+     */
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+}
