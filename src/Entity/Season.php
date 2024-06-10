45fd08f4dcbf6a4b2dc877fdf9f8f7508be58a59
Mathys Langiny <mathys.langiny@etudiant.univ-reims.fr>
2024-06-10 09:43:01 +0200

    Création de la classe Season.php avec ses attributs et les méthodes d'accès et de modification

---
+++ src/Entity/Season.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+class Season
+{
+    private int $id;
+    private int $tvShowId;
+    private string $name;
+    private int $seasonNumber;
+    private int $posterId;
+
+    /**
+     * Accesseur de l'Id de la classe season. Renvoie un entier.
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Mutateurs de l'id de la classe Season, prend en paramètre un entier.
+     *
+     * @param int $id
+     * @return void
+     */
+    private function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Accesseur de l'Id de la série Tv de la classe season. Renvoie un entier.
+     *
+     * @return int
+     */
+    public function getTvShowId(): int
+    {
+        return $this->tvShowId;
+    }
+
+    /**
+     * Mutateurs de l'id de la classe Season, prend en paramètre un entier.
+     *
+     * @param int $id
+     * @return void
+     */
+    public function setTvShowId(int $tvShowId): void
+    {
+        $this->tvShowId = $tvShowId;
+    }
+
+    /**
+     * Accesseur du nom de la classe season. Renvoie un entier.
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Mutateurs du nom de la classe Season, prend en paramètre un string.
+     *
+     * @param string $name
+     * @return void
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Accesseur renvoyant le nombre de saisons de la classe season. Renvoie un entier.
+     *
+     * @return int
+     */
+    public function getSeasonNumber(): int
+    {
+        return $this->seasonNumber;
+    }
+
+    /**
+     * Mutateurs du nombre de saisons de la classe Season, prend en paramètre un entier.
+     *
+     * @param int $id
+     * @return void
+     */
+    public function setSeasonNumber(int $seasonNumber): void
+    {
+        $this->seasonNumber = $seasonNumber;
+    }
+
+    /**
+     * Accesseur de l'Id du poster de la classe season. Renvoie un entier.
+     *
+     * @return int
+     */
+    public function getPosterId(): int
+    {
+        return $this->posterId;
+    }
+
+    /**
+     * Mutateurs de l'id du poster de la classe Season, prend en paramètre un entier.
+     *
+     * @param int $id
+     * @return void
+     */
+    public function setPosterId(int $posterId): void
+    {
+        $this->posterId = $posterId;
+    }
+
+
+}
