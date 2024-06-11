05f786cbb90fd53c45627ac5071019b0b3d9086c
Matteo Jadot-Dufour <matteo.jadot-dufour@etudiant.univ-reims.fr>
2024-06-11 09:48:18 +0200

    Modification : Préparation de la classe TvShow à la suppression d'une série
    Modifications d'une série :
    - L'identifiant est nullable
    - Les modificateurs sont fluides
    - Les accesseurs ont été régénérés
    - La méthode setId() est privée

--- src/Entity/TvShow.php
+++ src/Entity/TvShow.php
@@ -9,137 +9,77 @@ use PDO;
 
 class TvShow
 {
-    private int $id;
+    private ?int $id;
     private string $name;
     private string $originalName;
     private string $homepage;
     private string $overview;
     private int $posterId;
 
-    /**
-     * Accessseur de l'attribut id de la classe TvShow
-     *
-     * @return int L'identifiant du TvShow
-     */
-    public function getId(): int
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * Modificateur de l'attribut id de la classe TvShow
-     *
-     * @param int $id L'identifiant du TvShow
-     * @return void
-     */
-    private function setId(int $id): void
+    private function setId(?int $id): TvShow
     {
         $this->id = $id;
+        return $this;
     }
 
-    /**
-     * Accessseur de l'attribut name de la classe TvShow
-     *
-     * @return string Le nom du TvShow
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * Modificateur de l'attribut name de la classe TvShow
-     *
-     * @param string $name Le nom du TvShow
-     * @return void
-     */
-    public function setName(string $name): void
+    public function setName(string $name): TvShow
     {
         $this->name = $name;
+        return $this;
     }
 
-    /**
-     * Accessseur de l'attribut originalName de la classe TvShow
-     *
-     * @return string Le nom original du TvShow
-     */
     public function getOriginalName(): string
     {
         return $this->originalName;
     }
 
-    /**
-     * Modificateur de l'attribut originalName de la classe TvShow
-     *
-     * @param string $originalName Le nom original du TvShow
-     * @return void
-     */
-    public function setOriginalName(string $originalName): void
+    public function setOriginalName(string $originalName): TvShow
     {
         $this->originalName = $originalName;
+        return $this;
     }
 
-    /**
-     * Accessseur de l'attribut homepage de la classe TvShow
-     *
-     * @return string Le lien de streaming pour voir le TvShow
-     */
     public function getHomepage(): string
     {
         return $this->homepage;
     }
 
-    /**
-     * Modificateur de l'attribut homepage de la classe TvShow
-     *
-     * @param string $homepage Le lien de streaming pour voir le TvShow
-     * @return void
-     */
-    public function setHomepage(string $homepage): void
+    public function setHomepage(string $homepage): TvShow
     {
         $this->homepage = $homepage;
+        return $this;
     }
 
-    /**
-     * Accessseur de l'attribut overview de la classe TvShow
-     *
-     * @return string Le résumé du TvShow
-     */
     public function getOverview(): string
     {
         return $this->overview;
     }
 
-    /**
-     * Modificateur de l'attribut overview de la classe TvShow
-     *
-     * @param string $overview Le résumé du TvShow
-     * @return void
-     */
-    public function setOverview(string $overview): void
+    public function setOverview(string $overview): TvShow
     {
         $this->overview = $overview;
+        return $this;
     }
 
-    /**
-     * Accessseur de l'attribut posterId de la classe TvShow
-     *
-     * @return int L'identifiant du poster du TvShow
-     */
     public function getPosterId(): int
     {
         return $this->posterId;
     }
 
-    /**
-     * Modificateur de l'attribut posterId de la classe TvShow
-     *
-     * @param int $posterId Le résumé du TvShow
-     * @return void
-     */
-    public function setPosterId(int $posterId): void
+    public function setPosterId(int $posterId): TvShow
     {
         $this->posterId = $posterId;
+        return $this;
     }
 
     /**
