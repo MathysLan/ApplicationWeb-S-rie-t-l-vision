c3bfadabb228eef25ff083f9931ede74f9b6b64a
Matteo Jadot-Dufour <matteo.jadot-dufour@etudiant.univ-reims.fr>
2024-06-11 16:52:03 +0200

    Nouvelle fonctionnalité : Intégration du filtrage par genre sur la page d'accueil

--- public/index.php
+++ public/index.php
@@ -4,23 +4,50 @@ declare(strict_types=1);
 
 use Entity\Collection\GenreCollection;
 use Entity\Collection\TvshowCollection;
+use Entity\Exception\EntityNotFoundException;
+use Entity\Genre;
 use Html\AppWebPage;
+
+try {
+    if (isset($_GET['genreId']) && ctype_digit($_GET['genreId'])) {
+        $genre = Genre::findById((int) $_GET['genreId']);
+        $tvShowCollection = $genre->getTvShow();
+    } else {
+        $genre = null;
+        $tvShowCollection = TvshowCollection::findAll();
+    }
+} catch(EntityNotFoundException) {
+    http_response_code(404);
+    header('Location: /');
+}
 
 $webPage = new AppWebPage("Séries TV");
 
-$webPage->appendContent(<<<HTML
+// A COMPACTER
+if ($genre === null) {
+    $webPage->appendContent(<<<HTML
     <nav>
         <a href='admin/tvShow-form.php'>Ajouter</a>
     </nav>
     <div class='filter'>
 HTML);
+    foreach(GenreCollection::findAll() as $genre) {
+        $webPage->appendContent("<p><a href='index.php?genreId={$genre->getId()}'>{$genre->getName()}</a></p>");
+    }
 
-foreach(GenreCollection::findAll() as $genre) {
-    $webPage->appendContent("<p>{$genre->getName()}</p>");
+} else {
+    $webPage->appendContent(<<<HTML
+    <nav>
+        <a href='/'>Réinitialiser</a>
+        <a href='admin/tvShow-form.php'>Ajouter</a>
+    </nav>
+    <div class='filter'>
+HTML);
+    $webPage->setTitle("Séries TV - Genre {$genre->getName()}");
 }
 
-$webPage->appendContent("</div><div class='list'></div>");
-foreach(TvshowCollection::findAll() as $tvShow) {
+$webPage->appendContent("</div><div class='list'>");
+foreach($tvShowCollection as $tvShow) {
     $webPage->appendContent(<<<HTML
         <a href="tvshow.php?tvShowId={$tvShow->getId()}">
         <div class="tvShow">
