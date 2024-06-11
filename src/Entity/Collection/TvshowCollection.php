1b989bfb4fb9cc8f6818b93532fa924682f6d661
Mathys Langiny <mathys.langiny@etudiant.univ-reims.fr>
2024-06-11 16:12:54 +0200

    Ajout: Ajout de la fonction findByGenreId dans la classe TvshowCollection.php

--- src/Entity/Collection/TvshowCollection.php
+++ src/Entity/Collection/TvshowCollection.php
@@ -11,7 +11,7 @@ use Entity\TvShow;
 class TvshowCollection
 {
     /**
-     * Méthode static renvoyant toutes les séries tv avec leur id, leur nom, leur nom original, le lien vers le site où l'on peut regarder la sérié,
+     * Méthode static renvoyant toutes les séries tv avec leur id, leur nom, leur nom original, le lien vers le site où l'on peut regarder la série,
      * la description et l'id du poster associé.
      * @return TvShow[]
      */
@@ -26,5 +26,27 @@ class TvshowCollection
         );
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_CLASS, "Entity\TvShow");
+    }
+
+    /**
+     * Méthode static renvoyant toutes les séries par rapport à un Id de genre avec leur id, leur nom, leur nom original, le lien vers le site où l'on peut regarder la série,
+     *
+     * @param int $genreId Le genre de l'Id ou l'on veut les séries associées.
+     * @return TvShow[]
+     */
+    public static function findByGenreId(int $genreId): array
+    {
+        $req = MyPDO::getInstance()->prepare(
+            <<<SQL
+            SELECT id, name, originalName, homepage, overview, posterId
+            FROM tvshow
+            WHERE id = (SELECT tvShowId
+                        FROM tvshow_genre
+                        WHERE genreId = :genreId)
+            ORDER BY name
+        SQL
+        );
+        $req->execute(['$genreId' => $genreId]);
+        return $req->fetchAll(PDO::FETCH_CLASS, "Entity\TvShow");
     }
 }
