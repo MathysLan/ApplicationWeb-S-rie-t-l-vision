d1c798dff5f599e1eef07305b4a416849965b27a
Mathys Langiny <mathys.langiny@etudiant.univ-reims.fr>
2024-06-11 10:47:43 +0200

    Ajout: Ajout de la méthode getHtmlForm($action) qui permet de renvoyer un formulaire pour la création ou la modification d'un tvShow

--- src/Html/Form/TvShowForm.php
+++ src/Html/Form/TvShowForm.php
@@ -31,6 +31,28 @@ class TvShowForm
         return $this->tvShow;
     }
 
-
+    /**
+     * Méthode permettant de récupérer un formulaire html avec l'action passé en paramètre
+     *
+     * @param string $action
+     * @return string
+     */
+    public function getHtmlForm(string $action): string
+    {
+        $nom = $this->escapeString($this?->getTvShow()?->getName());
+        $originalName = $this->escapeString($this?->getTvShow()->getOriginalName());
+        $homepage = $this->escapeString($this?->getTvShow()->getHomepage());
+        $overview = $this->escapeString($this?->getTvShow()->getOverview());
+        return <<<HTML
+        <form method="post" action="$action">
+            <input type="hidden" name="id" value="{$this?->getTvShow()?->getId()}"> 
+            <input type="text" name="name" value="$nom" required="required">
+            <input type="text" name="originalName" value="$originalName" required="required">
+            <input type="url" name="homepage" value="$homepage" >
+            <input type="text" name="overview" value="$overview"  required="required">
+            <label for="name">Nom</label><button type="submit">Enregistrer</button>
+        </form>
+        HTML;
+    }
 
 }
