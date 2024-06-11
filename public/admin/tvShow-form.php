97b72177290ef918204c8f3c0ea4c0920317f6f6
Mathys Langiny <mathys.langiny@etudiant.univ-reims.fr>
2024-06-11 11:29:15 +0200

    Création: Création du formulaire de saisie pour la série

---
+++ public/admin/tvShow-form.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+use Entity\TvShow;
+use Html\Form\TvShowForm;
+use Entity\Exception\EntityNotFoundException;
+use Entity\Exception\ParameterException;
+
+try {
+
+    if (!isset($_GET['tvShowId'])) {
+        $tvShow = null;
+    } else {
+        if (!(is_numeric($_GET['tvShowId']))) {
+            throw new ParameterException();
+        }
+        $tvShowId = (int)$_GET['$tvShowId'];
+        $tvShow = TvShow::findById($tvShowId);
+
+    }
+
+    $tvShowForm = new TvShowForm($tvShow);
+    echo $tvShowForm->getHtmlForm('tvShow-save.php');
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (Exception) {
+    http_response_code(500);
+}
