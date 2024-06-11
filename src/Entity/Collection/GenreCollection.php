655e1a600e31134d78e14513924123ff20b45d15
Mathys Langiny <mathys.langiny@etudiant.univ-reims.fr>
2024-06-11 15:11:14 +0200

    AJout: Création de la classe GenreCollection et ça méthode statique findAll()

---
+++ src/Entity/Collection/GenreCollection.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\Genre;
+use PDO;
+
+class GenreCollection
+{
+    /**
+     * Méthode static renvoyant tous les genres avec leur id et leur nom
+     *
+     * @return Genre[]
+     */
+    public static function findAll(): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<SQL
+            SELECT id, name
+            FROM genre
+            ORDER BY name
+         SQL
+        );
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS, "Entity\Genre");
+    }
+}
