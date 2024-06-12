7cc16e337947ede37245b2bb27bc3a32c4c2e537
Mathys Langiny <mathys.langiny@gmail.com>
2024-06-12 17:48:33 +0200

    Ajout: Ajout de commentaire pour expliquer les différentes lignes

--- src/Entity/Season.php
+++ src/Entity/Season.php
@@ -10,16 +10,17 @@ use PDO;
 
 class Season
 {
-    private int $id;
-    private int $tvShowId;
-    private string $name;
-    private int $seasonNumber;
-    private ?int $posterId;
+    private int $id; // Identifiant de la saison.
+    private int $tvShowId; // Identifiant de la série TV à laquelle la saison appartient.
+    private string $name; // Nom de la saison.
+    private int $seasonNumber; // Numéro de la saison.
+    private ?int $posterId; // Identifiant du poster de la saison, nullable.
+
 
     /**
      * Accesseur de l'Id de la classe season. Renvoie un entier.
      *
-     * @return int
+     * @return int Id de la saison
      */
     public function getId(): int
     {
@@ -29,7 +30,7 @@ class Season
     /**
      * Mutateurs de l'id de la classe Season, prend en paramètre un entier.
      *
-     * @param int $id
+     * @param int $id Nouveau id de la saison
      * @return void
      */
     private function setId(int $id): void
@@ -40,7 +41,7 @@ class Season
     /**
      * Accesseur de l'Id de la série Tv de la classe season. Renvoie un entier.
      *
-     * @return int
+     * @return int L'id de l'émission
      */
     public function getTvShowId(): int
     {
@@ -48,9 +49,9 @@ class Season
     }
 
     /**
-     * Mutateurs de l'id de la classe Season, prend en paramètre un entier.
+     * Mutateurs de l'id de l'émission associée à la saison, prend en paramètre un entier.
      *
-     * @param int $tvShowId
+     * @param int $tvShowId Nouveau id de l'émission
      * @return void
      */
     public function setTvShowId(int $tvShowId): void
@@ -61,7 +62,7 @@ class Season
     /**
      * Accesseur du nom de la classe season. Renvoie un entier.
      *
-     * @return string
+     * @return string Nom de la saison
      */
     public function getName(): string
     {
@@ -71,7 +72,7 @@ class Season
     /**
      * Mutateurs du nom de la classe Season, prend en paramètre un string.
      *
-     * @param string $name
+     * @param string $name Nouveau nom de la saison
      * @return void
      */
     public function setName(string $name): void
@@ -82,7 +83,7 @@ class Season
     /**
      * Accesseur renvoyant le nombre de saisons de la classe season. Renvoie un entier.
      *
-     * @return int
+     * @return int Nombre de saisons de la classe season
      */
     public function getSeasonNumber(): int
     {
@@ -92,7 +93,7 @@ class Season
     /**
      * Mutateurs du nombre de saisons de la classe Season, prend en paramètre un entier.
      *
-     * @param int $seasonNumber
+     * @param int $seasonNumber Le nouveau nombre de saisons
      * @return void
      */
     public function setSeasonNumber(int $seasonNumber): void
@@ -103,7 +104,7 @@ class Season
     /**
      * Accesseur de l'Id du poster de la classe season. Renvoie un entier.
      *
-     * @return int|null
+     * @return int|null L'id du poster associé ou null s'il n'en a pas
      */
     public function getPosterId(): ?int
     {
@@ -113,7 +114,7 @@ class Season
     /**
      * Mutateurs de l'id du poster de la classe Season, prend en paramètre un entier.
      *
-     * @param int|null $posterId
+     * @param int|null $posterId L'id du nouveau poster associé
      * @return void
      */
     public function setPosterId(?int $posterId): void
@@ -122,36 +123,36 @@ class Season
     }
 
     /**
-     * Récupère l'instance de la saison à partir de son identifiant
+     * Récupère une instance de saison à partir de son identifiant.
      *
-     * @param int $id L'identifiant de la saison
-     * @return Season L'instance de Season correspondant à la série
-     * @throws Exception\EntityNotFoundException
+     * @param int $id L'identifiant de la saison à récupérer.
+     * @return Season L'instance de saison correspondant à l'identifiant.
+     * @throws Exception\EntityNotFoundException Si la saison n'est pas trouvée dans la base de données.
      */
     public static function findById(int $id): Season
     {
-        $req = MyPDO::getInstance()->prepare(
+        $req = MyPdo::getInstance()->prepare(
             <<<SQL
             SELECT id, tvShowId, name, seasonNumber, posterId
             FROM season
             WHERE id = :seasonId
         SQL
-        );
-        $req->execute(['seasonId' => $id]);
-        $req->setFetchMode(PDO::FETCH_CLASS, 'Entity\Season');
-        if (($ligne = $req->fetch()) === false) {
-            throw new Exception\EntityNotFoundException();
+        ); // Préparation de la requête SQL pour sélectionner une saison par son identifiant.
+        $req->execute(['seasonId' => $id]); // Exécution de la requête avec l'identifiant de la saison.
+        $req->setFetchMode(PDO::FETCH_CLASS, 'Entity\Season'); // Définition du mode de récupération des résultats comme une instance de Season.
+        if (($ligne = $req->fetch()) === false) { // Vérification si aucune ligne n'est retournée.
+            throw new Exception\EntityNotFoundException(); // Lancer une exception si la saison n'est pas trouvée.
         }
-        return $ligne;
+        return $ligne; // Retourner l'instance de la saison trouvée.
     }
 
     /**
-     * Liste les épisodes de la saison
+     * Liste les épisodes de la saison.
      *
-     * @return Episode[] Liste des épisodes de la saison
+     * @return Episode[] Liste des épisodes de la saison.
      */
     public function getEpisodes(): array
     {
-        return EpisodeCollection::findBySeasonId($this->getId());
+        return EpisodeCollection::findBySeasonId($this->getId()); // Appeler la méthode findBySeasonId de EpisodeCollection pour récupérer les épisodes de cette saison.
     }
 }
