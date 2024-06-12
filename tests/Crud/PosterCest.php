23dbcb35ad570a1dacd03803e7b2b7ab3887c18e
Matteo Jadot-Dufour <matteo.jadot-dufour@etudiant.univ-reims.fr>
2024-06-12 11:02:35 +0200

    Tests : Tests de la classe Poster (réussis)

---
+++ tests/Crud/PosterCest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Crud;
+
+use Entity\Exception\EntityNotFoundException;
+use Entity\Poster;
+use Tests\CrudTester;
+
+class PosterCest
+{
+    public function findById(CrudTester $I)
+    {
+        $poster = Poster::findById(237);
+        $I->assertSame(237, $poster->getId());
+        $I->assertSame(file_get_contents(codecept_data_dir() . '/cover/cover411.jpeg'), $poster->getJpeg());
+    }
+
+    public function findByIdThrowsExceptionIfCoverDoesNotExist(CrudTester $I)
+    {
+        $I->expectThrowable(EntityNotFoundException::class, function () {
+            Poster::findById(PHP_INT_MAX);
+        });
+    }
+}
