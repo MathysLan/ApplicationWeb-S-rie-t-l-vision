7a99f8a5c24e8059527c0dd40e1a4b1f924a8015
Matteo Jadot-Dufour <matteo.jadot-dufour@etudiant.univ-reims.fr>
2024-06-12 10:24:53 +0200

    Tests : Tests de la méthode delete de TvShow (réussis)

--- tests/Crud/TvShowCest.php
+++ tests/Crud/TvShowCest.php
@@ -27,4 +27,22 @@ class TvShowCest
             TvShow::findById(PHP_INT_MAX);
         });
     }
+
+    public function delete(CrudTester $I)
+    {
+        $artist = TvShow::findById(57);
+        $artist->delete();
+        $I->cantSeeInDatabase('tvshow', ['id' => 57]);
+        $I->cantSeeInDatabase('tvshow', ['name' => 'Good Omens']);
+        $I->cantSeeInDatabase('tvshow', ['originalName' => 'Good Omens']);
+        $I->cantSeeInDatabase('tvshow', ['homepage' => 'https://www.amazon.com/dp/B07FMHTRFF']);
+        $I->cantSeeInDatabase('tvshow', ['overview' => 'Suit les aventures de Crowley et Aziraphale, un démon et un ange, qui décident de saborder l’Apocalypse car ils se sont trop habitués à la vie sur Terre.']);
+        $I->cantSeeInDatabase('tvshow', ['posterId' => 466]);
+        $I->assertNull($artist->getId());
+        $I->assertSame('Good Omens', $artist->getName());
+        $I->assertSame('Good Omens', $artist->getOriginalName());
+        $I->assertSame('https://www.amazon.com/dp/B07FMHTRFF', $artist->getHomepage());
+        $I->assertSame('Suit les aventures de Crowley et Aziraphale, un démon et un ange, qui décident de saborder l’Apocalypse car ils se sont trop habitués à la vie sur Terre.', $artist->getOverview());
+        $I->assertSame(466, $artist->getPosterId());
+    }
 }
