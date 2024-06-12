0f973d9377e02a91402c1762b8a36b71a7879522
Mathys Langiny <mathys.langiny@gmail.com>
2024-06-12 17:39:35 +0200

    Ajout: Ajout de commentaire pour expliquer les différentes lignes

--- src/Entity/Collection/EpisodeCollection.php
+++ src/Entity/Collection/EpisodeCollection.php
@@ -13,7 +13,8 @@ class EpisodeCollection
     /**
      * Méthode static renvoyant tous les épisodes avec leur id, leur id de saison, leur nom, leur résumé et le numéro de l'épisode.
      *
-     * @return Episode[]
+     * @param int $SeasonId L'identifiant de la saison pour laquelle on veut récupérer les épisodes.
+     * @return Episode[] Retourne un tableau d'objets Episode.
      */
     public static function findBySeasonId(int $SeasonId): array
     {
@@ -24,8 +25,8 @@ class EpisodeCollection
             WHERE seasonId = :seasonId
             ORDER BY episodeNumber
         SQL
-        );
-        $req->execute(['seasonId' => $SeasonId]);
-        return $req->fetchAll(PDO::FETCH_CLASS, "Entity\Episode");
+        ); // Préparer une requête SQL pour sélectionner les épisodes d'une saison donnée, triés par numéro d'épisode.
+        $req->execute(['seasonId' => $SeasonId]); // Exécuter la requête avec l'identifiant de la saison fourni.
+        return $req->fetchAll(PDO::FETCH_CLASS, "Entity\Episode"); // Récupérer les résultats sous forme d'objets de la classe Episode et les retourner.
     }
 }
