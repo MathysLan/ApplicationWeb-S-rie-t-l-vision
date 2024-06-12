6143dccd2484e69ad5b751eebe8450dd513f9650
Mathys Langiny <mathys.langiny@gmail.com>
2024-06-12 17:39:47 +0200

    Ajout: Ajout de commentaire pour expliquer les différentes lignes

--- src/Entity/Collection/SeasonCollection.php
+++ src/Entity/Collection/SeasonCollection.php
@@ -13,8 +13,8 @@ class SeasonCollection
     /**
      * Méthode static renvoyant toutes les saisons avec leur id, leur id d'émission, leur nom, leur numéro de saison et le numéro du poster associé.
      *
-     * @param int $tvShowId
-     * @return Season[]
+     * @param int $tvShowId L'identifiant de la série TV pour laquelle on veut récupérer les saisons.
+     * @return Season[] Retourne un tableau d'objets Season.
      */
     public static function findByTvShowId(int $tvShowId): array
     {
@@ -25,8 +25,8 @@ class SeasonCollection
             WHERE tvShowId = :tvShowId
             ORDER BY seasonNumber
         SQL
-        );
-        $req->execute(['tvShowId' => $tvShowId]);
-        return $req->fetchAll(PDO::FETCH_CLASS, "Entity\Season");
+        ); // Préparer une requête SQL pour sélectionner les saisons d'une série TV donnée, triées par numéro de saison.
+        $req->execute(['tvShowId' => $tvShowId]); // Exécuter la requête avec l'identifiant de la série TV fourni.
+        return $req->fetchAll(PDO::FETCH_CLASS, "Entity\Season"); // Récupérer les résultats sous forme d'objets de la classe Season et les retourner.
     }
 }
