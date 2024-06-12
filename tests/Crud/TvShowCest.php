0c685f6b25e851b7e908e8e332c986ee7990173e
Matteo Jadot-Dufour <matteo.jadot-dufour@etudiant.univ-reims.fr>
2024-06-12 10:41:34 +0200

    Tests : Tests de la méthode update de TvShow (réussis)

--- tests/Crud/TvShowCest.php
+++ tests/Crud/TvShowCest.php
@@ -12,13 +12,13 @@ class TvShowCest
 {
     public function findById(CrudTester $I)
     {
-        $artist = TvShow::findById(57);
-        $I->assertSame(57, $artist->getId());
-        $I->assertSame('Good Omens', $artist->getName());
-        $I->assertSame('Good Omens', $artist->getOriginalName());
-        $I->assertSame('https://www.amazon.com/dp/B07FMHTRFF', $artist->getHomepage());
-        $I->assertSame('Suit les aventures de Crowley et Aziraphale, un démon et un ange, qui décident de saborder l’Apocalypse car ils se sont trop habitués à la vie sur Terre.', $artist->getOverview());
-        $I->assertSame(466, $artist->getPosterId());
+        $tvShow = TvShow::findById(57);
+        $I->assertSame(57, $tvShow->getId());
+        $I->assertSame('Good Omens', $tvShow->getName());
+        $I->assertSame('Good Omens', $tvShow->getOriginalName());
+        $I->assertSame('https://www.amazon.com/dp/B07FMHTRFF', $tvShow->getHomepage());
+        $I->assertSame('Suit les aventures de Crowley et Aziraphale, un démon et un ange, qui décident de saborder l’Apocalypse car ils se sont trop habitués à la vie sur Terre.', $tvShow->getOverview());
+        $I->assertSame(466, $tvShow->getPosterId());
     }
 
     public function findByIdThrowsExceptionIfArtistDoesNotExist(CrudTester $I)
@@ -30,19 +30,44 @@ class TvShowCest
 
     public function delete(CrudTester $I)
     {
-        $artist = TvShow::findById(57);
-        $artist->delete();
+        $tvShow = TvShow::findById(57);
+        $tvShow->delete();
         $I->cantSeeInDatabase('tvshow', ['id' => 57]);
         $I->cantSeeInDatabase('tvshow', ['name' => 'Good Omens']);
         $I->cantSeeInDatabase('tvshow', ['originalName' => 'Good Omens']);
         $I->cantSeeInDatabase('tvshow', ['homepage' => 'https://www.amazon.com/dp/B07FMHTRFF']);
         $I->cantSeeInDatabase('tvshow', ['overview' => 'Suit les aventures de Crowley et Aziraphale, un démon et un ange, qui décident de saborder l’Apocalypse car ils se sont trop habitués à la vie sur Terre.']);
         $I->cantSeeInDatabase('tvshow', ['posterId' => 466]);
-        $I->assertNull($artist->getId());
-        $I->assertSame('Good Omens', $artist->getName());
-        $I->assertSame('Good Omens', $artist->getOriginalName());
-        $I->assertSame('https://www.amazon.com/dp/B07FMHTRFF', $artist->getHomepage());
-        $I->assertSame('Suit les aventures de Crowley et Aziraphale, un démon et un ange, qui décident de saborder l’Apocalypse car ils se sont trop habitués à la vie sur Terre.', $artist->getOverview());
-        $I->assertSame(466, $artist->getPosterId());
+        $I->assertNull($tvShow->getId());
+        $I->assertSame('Good Omens', $tvShow->getName());
+        $I->assertSame('Good Omens', $tvShow->getOriginalName());
+        $I->assertSame('https://www.amazon.com/dp/B07FMHTRFF', $tvShow->getHomepage());
+        $I->assertSame('Suit les aventures de Crowley et Aziraphale, un démon et un ange, qui décident de saborder l’Apocalypse car ils se sont trop habitués à la vie sur Terre.', $tvShow->getOverview());
+        $I->assertSame(466, $tvShow->getPosterId());
+    }
+
+    public function update(CrudTester $I)
+    {
+        $tvShow = TvShow::findById(57);
+        $tvShow->setName('Noeud Coulant');
+        $tvShow->setOriginalName('Noeud Coulant');
+        $tvShow->setHomepage('https://iut-info.univ-reims.fr');
+        $tvShow->setOverview('Noeud Coulant');
+        $tvShow->setPosterId(null);
+        $tvShow->save();
+        $I->canSeeNumRecords(1, 'tvshow', [
+            'id' => 57,
+            'name' => 'Noeud Coulant',
+            'originalName' => 'Noeud Coulant',
+            'homepage' => 'https://iut-info.univ-reims.fr',
+            'overview' => 'Noeud Coulant',
+            'posterId' => null
+        ]);
+        $I->assertSame(57, $tvShow->getId());
+        $I->assertSame('Noeud Coulant', $tvShow->getName());
+        $I->assertSame('Noeud Coulant', $tvShow->getOriginalName());
+        $I->assertSame('https://iut-info.univ-reims.fr', $tvShow->getHomepage());
+        $I->assertSame('Noeud Coulant', $tvShow->getOverview());
+        $I->assertSame(null, $tvShow->getPosterId());
     }
 }
